004506321897beecf2c4a9944ffc20be459d6146
Ларькин Михаил <jetpilot314@gmail.com>
2023-03-07 20:22:47 +0300

    Initial commit

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import SimpleRouter
+
+router_v1 = SimpleRouter()
+# router_v1.register()
 
 
 app_name = "api"
 
 urlpatterns = [
-    path("v1/", views.smth)
+    path("v1/", include(router_v1.urls))
 ]
