e64f4dfe1c787cd282ec850a1b2747094a796d5a
Ларькин Михаил <jetpilot314@gmail.com>
2023-04-06 15:14:24 +0300

    Ver 1.2(testing pytest)

--- tests/test_smth.py
+++ tests/test_smth.py
@@ -4,13 +4,14 @@ import pytest
 def add(a, b):
     return a + b
 
-class TestSomeStuff:
 
-    @pytest.mark.parametrize("args, expected_result",
+class TestSomeStuff:
+    @pytest.mark.parametrize(
+        "args, expected_result",
         (
             ((1, 2), 3),
             ((2, 3), 5),
-        )
+        ),
     )
     def test_do_smth(self, args, expected_result):
         assert add(*args) == expected_result
